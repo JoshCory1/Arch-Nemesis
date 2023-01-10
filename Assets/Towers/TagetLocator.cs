02eb008d620205e3ef27916b0198d90d091a748c
Joshua Cory <accounts@keeplooking.net>
2023-01-10 10:08:48 -0700

    Refined target system

--- Assets/Towers/TagetLocator.cs
+++ Assets/Towers/TagetLocator.cs
@@ -6,21 +6,48 @@ using UnityEngine;
 public class TagetLocator : MonoBehaviour
 {
     [SerializeField] Transform Wepon;
+    [SerializeField] ParticleSystem ProjectileParticals;
+    [SerializeField] float range = 15f;
     [SerializeField] Transform Target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Target = FindObjectOfType<EnmeyMover>().transform;
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWepon();   
     }
 
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        Target = closestTarget;
+    }
     private void AimWepon()
     {
+        float targetDistance = Vector3.Distance(transform.position, Target.position);
         transform.LookAt(Target);
+        if(targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+    void Attack(bool IsActive)
+    {
+        var emissonModule = ProjectileParticals.emission;
     }
 }
